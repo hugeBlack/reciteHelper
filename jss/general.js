fe5f241e3836ffe6eff4b42e7b61858e65e2d530
BuildTools <wwkq03@outlook.com>
2020-01-24 22:55:04 +0800

    a

--- jss/general.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click','.container_closeBtn',function () { 
-    $(this).parent().remove();
-});
-
-$('.container_closeBtn').click(function () { 
-    $(this).parent().remove();
-});
--- jss/getPoems.php
+++
@@ -1,4 +0,0 @@
-<?php
-$poemJson = fopen("poemInfo.json","r") or die('nmsl');
-echo fread($poemJson,filesize("poemInfo.json"));
-?>
