1967c6b4d537932334c25c3550974c81793987eb
BuildTools <wwkq03@outlook.com>
2020-01-25 22:06:16 +0800

    licenseUpdate

--- windows/jss/testResult.js
+++ windows/jss/testResult.js
@@ -3,6 +3,7 @@ var result=window.parent.generalValues['testResult'];
 $('#totalCount').html(result.totalCount);
 $('#knowCount').html(result.knowCount);
 $('#forgetCount').html(result.forgetCount);
+$('#score').html(Math.round((1-result.forgetCount/result.totalCount)*100));
 
 $('#historyBtn').click(()=>{
     window.parent.newWindow('history');
